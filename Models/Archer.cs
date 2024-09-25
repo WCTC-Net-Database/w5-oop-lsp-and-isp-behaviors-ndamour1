5124f4fa093655e8c2de567bfbc0f4eb32d7db06
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-25 14:16:19 -0400

    Archer and Mage Classes

---
+++ Models/Archer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5_assignment_template.Interfaces;
+
+namespace W5_assignment_template.Models
+{
+    public class Archer : IEntity
+    {
+        public string Name { get; set; }
+
+        public void Attack(IEntity target)
+        {
+            Console.WriteLine($"{Name} attacks {target.Name}");
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"{Name} moves forward.");
+        }
+
+        public void Shoot(IEntity target)
+        {
+            Console.WriteLine($"{Name} shot {target.Name} with an arrow.");
+        }
+
+        public void Cast(IEntity target, string spellName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Mage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5_assignment_template.Interfaces;
+
+namespace W5_assignment_template.Models
+{
+    public class Mage : IEntity
+    {
+        public string Name { get; set; }
+
+        public void Attack(IEntity target)
+        {
+            Console.WriteLine($"{Name} attacks {target.Name}");
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"{Name} moves forward.");
+        }
+
+        public void Shoot(IEntity target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cast(IEntity target, string spellName)
+        {
+            Console.WriteLine($"{Name} cast {spellName} on {target.Name}.");
+        }
+    }
+}
