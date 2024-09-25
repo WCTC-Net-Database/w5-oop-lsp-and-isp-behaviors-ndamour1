92df5401ea299ac620b2663fd91721b7c4554a90
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-25 14:13:56 -0400

    Add files via upload

---
+++ Interfaces/ICastable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5_assignment_template.Interfaces
+{
+    public interface ICastable
+    {
+        void Cast();
+
+        public void CastSpell(IEntity target, string spellName);
+    }
+}
