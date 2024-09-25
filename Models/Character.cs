32057470ff116a78ff3a493b9252795d415cc7cb
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-25 14:14:35 -0400

    Update Character.cs

--- Models/Character.cs
+++ Models/Character.cs
@@ -5,19 +5,29 @@ namespace W5_assignment_template.Models;
 public class Character : IEntity
 {
     public string Name { get; set; }
-
+    
     public void Attack(IEntity target)
     {
         Console.WriteLine($"{Name} attacks {target.Name}");
     }
-
+    
     public void Fly()
     {
         throw new NotImplementedException();
     }
-
+    
     public void Move()
     {
         Console.WriteLine($"{Name} moves forward.");
+    }
+    
+    public void Shoot(IEntity target)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Cast(IEntity target, string spellName)
+    {
+        throw new NotImplementedException();
     }
 }
