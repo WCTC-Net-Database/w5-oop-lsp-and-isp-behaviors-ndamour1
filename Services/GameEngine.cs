c4d7378c1e2e14c74485e6c91814c8f53d87fa97
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-25 14:21:41 -0400

    Update GameEngine.cs

--- Services/GameEngine.cs
+++ Services/GameEngine.cs
@@ -6,30 +6,42 @@ namespace W5_assignment_template.Services
     public class GameEngine
     {
         private readonly IEntity _character;
+        private readonly IEntity _archer;
+        private readonly IEntity _mage;
         private readonly IEntity _goblin;
         private readonly IEntity _ghost;
 
-        public GameEngine(IEntity character, IEntity goblin, IEntity ghost)
+        public GameEngine(IEntity character, IEntity archer, IEntity mage, IEntity goblin, IEntity ghost)
         {
             _character = character;
+            _archer = archer;
+            _mage = mage;
             _goblin = goblin;
             _ghost = ghost;
         }
 
         public void Run()
         {
-            _character.Name = "Hero";
-            _goblin.Name = "Goblin";
-            _ghost.Name = "Ghost";
+            _character.Name = "Blast Hardcheese";
+            _archer.Name = "Hawkeye";
+            _mage.Name = "Doctor Fate";
+            _goblin.Name = "Snorg";
+            _ghost.Name = "Z'Skayr";
 
             _character.Move();
             _character.Attack(_goblin);
+
+            _archer.Move();
+            ((Archer)_archer).Shoot(_goblin);
+
+            _mage.Move();
+            ((Mage)_mage).Cast(_ghost, "Crush");
 
             _goblin.Move();
             _goblin.Attack(_character);
 
             _ghost.Move();
-            _ghost.Attack(_character);
+            _ghost.Attack(_mage);
             ((Ghost) _ghost).Fly();
         }
     }
