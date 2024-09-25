662cee22befea7acd87b1d2206eb64586696afa0
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-25 14:15:15 -0400

    Update Ghost.cs

--- Models/Ghost.cs
+++ Models/Ghost.cs
@@ -5,20 +5,21 @@ namespace W5_assignment_template.Models
     public class Ghost : IEntity
     {
         public string Name { get; set; }
-
+        public IFlyable Flyable { get; set; }
+        
         public void Attack(IEntity target)
         {
             Console.WriteLine($"{Name} attacks {target.Name} with a chilling touch.");
         }
-
+        
         public void Move()
         {
             Console.WriteLine($"{Name} floats silently.");
         }
-
+        
         public void Fly()
         {
-            Console.WriteLine($"{Name} flies rapidly through the air.");
+            Console.WriteLine($"{Name} can fly.");
         }
     }
 }
